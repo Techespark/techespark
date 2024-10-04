bacdcbea8440f8353f71e408ac1d27a8c06abe00
Techespark <techespark1@gmail.com>
2024-10-04 09:30:34 +0530

    New files updated

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add paths to all your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
